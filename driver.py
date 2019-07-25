3b87a54c9ea23ced068a1dce357e3a8ecd751bf0
Scarlet <940012978@qq.com>
2019-07-26 03:57:19 +0800

    remote?

---
+++ driver.py
@@ -0,0 +1,138 @@
+import socket
+import time
+import json
+class suibianxiexie:
+    'zhe sha'
+    def __init__(self):
+        self.mode="remote"
+        self.jogging=0
+        self.left=0
+        self.right=0
+        self.up=0
+        self.down=0
+        self.X=0
+        self.Y=0
+    def stop(self):
+        print("stop")
+        self.__init__()
+    def __jog(self):
+        print("jog")
+        pass
+    def __f(self):      #forward
+        print("forward")
+        pass
+    def __b(self):      #back
+        print("back")
+        pass
+    def __fl(self):     #forward-left
+        print("forward-left")
+        pass
+    def __fr(self):     #forward-right
+        print("forward-right")
+        pass
+    def __bl(self):     #back-left
+        print("back-left")
+        pass
+    def __br(self):     #back-right
+        print("back-right")
+        pass
+    def __zibi(self):   #turn round
+        print("zibi")
+        pass
+    def __remote(self):
+        if (self.jogging):
+            self.__jog()
+        if(self.up and self.left):
+            self.__fl()
+        elif (self.up and self.right):
+            self.__fr()
+        elif (self.up):
+            self.__f()
+        elif(self.down and self.left):
+            self.__bl()
+        elif (self.down and self.right):
+            self.__br()
+        elif (self.down):
+            self.__b()
+    def __trace(self):
+        if(self.X>0):
+            if(self.X<320/2):
+                self.__fl()
+            else:
+                self.__fr()
+        else:
+            self.__zibi()
+    def perform(self):
+        if (self.mode=="remote"):
+            self.__remote()
+        if (self.mode=="trace"):
+            self.__trace()
+    def process(self,ins):
+        self.X,self.Y=ins[0]["x"],ins[0]["y"]
+        k=ins[0]["key"]
+        if k==ord("w"):
+            print("w")
+            self.up=1
+            self.down=0
+            self.left=0
+            self.right=0
+        elif k==ord("s"):
+            print("s")
+            self.up=0
+            self.down=1
+            self.left = 0
+            self.right = 0
+        elif k==ord("a"):
+            print("a")
+            self.left=1
+            self.right=0
+        elif k==ord("d"):
+            print("d")
+            self.left=0
+            self.right=1
+        elif k==ord(" "):
+            print("Space")
+            self.stop()
+        elif k==ord("j"):
+            print("j")
+            self.jogging = 1
+        elif k==ord("k"):
+            print("k")
+            self.jogging = 0
+        elif k==ord("t"):
+            print("t")
+            self.mode = "trace"
+        elif k==ord("r"):
+            print("r")
+            self.mode="remote"
+        self.perform()
+ktt = suibianxiexie()
+def process(data):
+    global x,y,ktt
+    jdata=json.loads(data)
+    ktt.process(jdata)
+    #print("x:{},y:{}".format(jdata[0]["x"],jdata[0]["y"]))
+while 1:
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('192.168.1.109', 9920))
+    server.listen(5)
+    print("waiting")
+    connect, addr = server.accept()
+    print("{} connected".format(addr))
+    while 1:
+        try:
+            data=connect.recv(1024)
+        except:
+            print("connection failed!!!")
+            break
+        if not data:
+            try:
+                connect.sendall("What's your problem?")
+            except:
+                break
+        else:
+            process(data)
+        #print("Received:{}\r".format(data))
+    server.close()
+    ktt.stop()
--- server.py
+++ server.py
@@ -3,37 +3,36 @@ from picamera import PiCamera
 import socket
 import time
 import cv2
-
+sz = (320, 240)
 def initCamera():
     camera = PiCamera()
-    camera.resolution = (640, 480)
+    camera.resolution = sz
     camera.framerate = 32
     camera.iso = 800
     camera.brightness = 70
-    rawCapture = PiRGBArray(camera, size=(640, 480))
+    rawCapture = PiRGBArray(camera, size=sz)
     time.sleep(0.1)
     return rawCapture,camera
 
 rawCapture,camera=initCamera()
 while 1:
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    qwq=('192.168.1.104',10000)
+    rawCapture = PiRGBArray(camera, size=sz)
     server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind(qwq)
+    server.bind(('192.168.1.109',10010))
     server.listen(5)
     print("waiting")
     connect,addr=server.accept()
     print("{} connected".format(addr))
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         image = frame.array
-        ret,imgencode=cv2.imencode('.jpg',image,[cv2.IMWRITE_JPEG_QUALITY,50])
+        #ret, imgencode = cv2.imencode('.jpg', image, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        ret, imgencode = cv2.imencode('.jpg', image)
         try:
             connect.sendall(imgencode)
-            data=connect.recv(1024)
         except:
             print("connection failed!!!")
             break
-        print("Received:{}\r".format(data))
+        #print("Received:{}\r".format(data))
         rawCapture.truncate(0)
     server.close()
 
